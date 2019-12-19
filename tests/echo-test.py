e36691ee81eacdbfc358a4e574e2d3544718fcc3
Mario Gonzalez <mario.gonzalez@placetopay.com>
2019-12-19 18:32:04 -0500

    feat: change TPDU in echo test

--- tests/echo-test.py
+++ tests/echo-test.py
@@ -5,26 +5,37 @@ from iso8583.specs import default
 
 
 def generate_network_message():
+    default['h'] = {
+        "data_enc": "b",
+        "len_enc": "b",
+        "len_type": 0,
+        "max_len": 7,
+        "desc": "Message Header"
+    }
     doc_dec = {}
     iso8583.add_field(doc_dec, 't', '0800')
     iso8583.add_field(doc_dec, '11', '000001')
     iso8583.add_field(doc_dec, '37', '001234567910')
     iso8583.add_field(doc_dec, '70', '270')
+    iso8583.add_field(doc_dec, 'h', "002E6000018000")
 
     return iso8583.encode(doc_dec, spec=default)
 
 
 if __name__ == '__main__':
-    message, _ = generate_network_message()
-    print(type(message))
+    message, dec = generate_network_message()
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('Connecting to Host:{host} and Port:{port}'.format(host=CONNECTION_SETTINGS['host'],
                                                              port=CONNECTION_SETTINGS['port']))
     sock.connect((CONNECTION_SETTINGS['host'], CONNECTION_SETTINGS['port']))
-    message = '0046'.encode('ascii') + '`'.encode('ascii') + '000000'.encode('ascii') + message
     sock.sendall(message)
-
+    bytearray = bytearray([0x00, 0x2e, 0x60, 0x00, 0x01, 0x80, 0x00, 0x30,
+                          0x38, 0x30, 0x30, 0x80, 0x20, 0x00, 0x00, 0x08,
+                          0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
+                          0x00, 0x00, 0x00, 0x30, 0x30, 0x30, 0x30, 0x30,
+                          0x31, 0x30, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
+                          0x36, 0x37, 0x39, 0x31, 0x30, 0x32, 0x37, 0x30])
     data = b''
     while True:
         print('Receiving data...')
